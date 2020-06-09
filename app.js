71f9abef29380c85d50ea93974de1208492e6801
michaelh9013 <64647620+michaelh9013@users.noreply.github.com>
2020-06-08 20:59:24 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-//Bugs - when player two wins, attack button is not disabled
-//health div wont center in CSS
+// buff buttons need to be disabled when game ends
 //When reset button pressed mult times, attack buttons disabled
-//unable to change background element when winner class applied... not sure why 
 
-var health, whichPlayer, attackValue, turnCount, activeGame, resetCounter;
+let health,
+  activeGame,
+  activeBuffs,
+  whichPlayer,
+  attackValue,
+  turnCount,
+  resetCounter,
+  buffs,
+  buffCount,
+  buffHeart,
+  buffShield,
+  buffStrength,
+  hitValue,
+  healthCount,
+  shieldCount,
+  strengthCount;
 
 resetGame(); //load all game presets
 
@@ -20,9 +33,9 @@ document.getElementById("reset").addEventListener("click", resetGame); // reset 
  * ***************/
 
 function attack() {
-  var attackValue = Math.floor((Math.random() * 10)); //Set the attackValue
-
   if (whichPlayer === 0) {
+    attackValue = Math.floor(Math.random() * hitValue[whichPlayer]); //Set the attackValue
+
     health[1] = health[1] - attackValue; //Depending on the current player, remove attackValue from health of opposing player
 
     if (health[1] <= 0) {
@@ -36,13 +49,17 @@ function attack() {
 
       document.getElementById(`meter-1`).value = health[1];
 
+      document.getElementById(`health-num-1`).textContent = health[1]; // health numerical display
+
       endGame();
 
       return; // end function early
     }
 
     document.getElementById(`meter-1`).value = health[1];
-    
+
+    document.getElementById(`health-num-1`).textContent = health[1]; // health numerical display
+
     console.log(`(Player 1) hit a ${attackValue}.`);
 
     console.log(`Player 2 has  ${health[1]} health left.`);
@@ -57,9 +74,12 @@ function attack() {
       nextPlayer();
     }
   } else {
+    attackValue = Math.floor(Math.random() * hitValue[whichPlayer]); //Set the attackValue
+
     health[0] = health[0] - attackValue; //Player 2's turn
 
     if (health[0] <= 0) {
+      health[0] = 0;
 
       console.log(`(Player 2) hit a ${attackValue}`);
 
@@ -69,12 +89,16 @@ function attack() {
 
       document.getElementById(`meter-0`).value = health[0];
 
+      document.getElementById(`health-num-0`).textContent = health[0]; // health numerical display
+
       endGame();
 
       return; //end function early
     }
 
     document.getElementById(`meter-0`).value = health[0];
+
+    document.getElementById(`health-num-0`).textContent = health[0]; // health numerical display
 
     console.log(`(Player 2) hit a ${attackValue}`);
 
@@ -90,6 +114,7 @@ function attack() {
       nextPlayer();
     }
   }
+  buffControl(); //buff conditions checked on every click
 }
 
 /***********
@@ -99,13 +124,19 @@ function attack() {
  *********/
 
 function resetGame() {
-  health = [100, 100];
+  getBuffs(); // enable buff buttons
 
+  health = [100, 100];
+  (buffHeart = [false, false]),
+    (buffShield = [false, false]),
+    (buffStrength = [false, false]),
+    (hitValue = [10, 10]),
+    (healthCount = [0, 0]),
+    (shieldCount = [0, 0]),
+    (strengthCount = [0, 0]);
   turnCount = 0;
 
   whichPlayer = Math.floor(Math.random() * 2); // choose randomly which player to go first
-
-  console.log(whichPlayer);
 
   document
     .getElementById(`player-${whichPlayer}`)
@@ -120,8 +151,10 @@ function resetGame() {
     document.getElementById(`attack-1`).disabled = false;
   }
   // update the health meters on reset
-  document.getElementById(`meter-0`).setAttribute('value', health[0]);
-  document.getElementById(`meter-1`).setAttribute('value', health[1]);
+  document.getElementById(`meter-0`).setAttribute("value", health[0]);
+  document.getElementById(`meter-1`).setAttribute("value", health[1]);
+  document.getElementById(`health-num-0`).textContent = health[1]; // health numerical display
+  document.getElementById(`health-num-1`).textContent = health[0]; // health numerical display
 
   //ensure winner class has been removed from previous game
   document.getElementById(`player-0`).classList.remove("winner");
@@ -130,9 +163,15 @@ function resetGame() {
   document.getElementById(`h2-player-0`).textContent = "Player 1";
   document.getElementById(`h2-player-1`).textContent = "Player 2";
 
-  toggleButtons();
-}
+  //set up icons for mini active buffs, opaque when not active, controlled
+  //dynamically through the buffControl() function
+  activeBuffs = document.querySelectorAll(`.icon-active-buff`);
+  for (var i = 0; i < activeBuffs.length; i++) {
+    activeBuffs[i].classList.add("inactiveBuff");
 
+    toggleButtons();
+  }
+}
 /************
  *
  * Choose the next Player
@@ -146,27 +185,24 @@ function nextPlayer() {
     document.getElementById(`attack-1`).disabled = false;
     document.getElementById(`attack-1`).addEventListener("click", attack); // 2nd player attack button active
 
+    document.getElementById(`attack-0`).disabled = true; // 1st player attack button disabled
+
     document.getElementById(`player-1`).classList.add(`activePlayer`);
     document.getElementById(`player-0`).classList.remove(`activePlayer`);
-
-    console.log(
-      `nextPlayer() called, var whichPlayer = ${whichPlayer}. Output should be Player 2`
-    );
   } else {
     whichPlayer--;
 
     document.getElementById(`attack-0`).disabled = false;
     document.getElementById(`attack-0`).addEventListener("click", attack); // 1st player attack button active
 
+    document.getElementById(`attack-1`).disabled = true; //2nd player attack button disabled
+
     document.getElementById(`player-0`).classList.add("activePlayer");
     document.getElementById(`player-1`).classList.remove("activePlayer");
-
-    console.log(
-      `nextPlayer() called, var whichPlayer = ${whichPlayer}. Output should be Player 1`
-    );
   }
 
   turnCount = 0;
+  console.log(`nextPlayer() called, var whichPlayer = ${whichPlayer}.`);
 }
 
 /*************
@@ -181,7 +217,9 @@ function endGame() {
     .classList.remove("activePlayer"); // remove activePlayer class
   document.getElementById(`player-${whichPlayer}`).classList.add("winner"); // apply winner class to winning player
   document.getElementById(`h2-player-${whichPlayer}`).textContent = "Winner!";
+
   toggleButtons();
+
   console.log(`endGame(), GAME OVER!`);
   return;
 }
@@ -198,14 +236,165 @@ function toggleButtons() {
     document.getElementById(`attack-1`).disabled = true;
   } else if (whichPlayer === 1 && health[0] > 0) {
     document.getElementById(`attack-0`).disabled = true;
-  } else if (
-    // toggles buttons at the end of the game
-    whichPlayer === 0 ||
-    (whichPlayer === 1 && health[0] === 0) ||
-    health[1] === 0
-  ) {
+  } else if (health[0] === 0 || health[1] === 0) {
     document.getElementById(`attack-0`).disabled = true;
     document.getElementById(`attack-1`).disablde = true;
     return;
+  }
+}
+
+function getBuffs() {
+  buffs = document.querySelectorAll(".buff");
+
+  for (var i = 0; i < buffs.length; i++) {
+    buffs[i].addEventListener("click", function () {
+      if (this.id === `heart-${[whichPlayer]}` && whichPlayer === whichPlayer) {
+        health[whichPlayer] = health[whichPlayer] + 50;
+        if (health[whichPlayer] > 100) {
+          health[whichPlayer] = 100;
+        }
+        document
+          .getElementById(`meter-${whichPlayer}`)
+          .setAttribute("value", health[whichPlayer]);
+        document.getElementById(`health-num-${whichPlayer}`).textContent =
+          health[whichPlayer];
+        document
+          .getElementById(`heart-${whichPlayer}`)
+          .classList.add("inactiveBuff");
+        buffHeart[whichPlayer] = true;
+        buffControl();
+        nextPlayer();
+      } else if (
+        this.id === `shield-${whichPlayer}` &&
+        whichPlayer === whichPlayer
+      ) {
+        if (whichPlayer === 0) {
+          hitValue[1] = 4; // lower the hitValue of offensive player for perceived gain in defense for the defensive player
+        } else {
+          hitValue[0] = 4;
+        }
+        console.log(`(Player ${whichPlayer}) selected the sheild buff`);
+        document
+          .getElementById(`shield-${whichPlayer}`)
+          .classList.add("inactiveBuff");
+        buffShield[whichPlayer] = true;
+        buffControl();
+        nextPlayer();
+      } else if (
+        this.id === `strength-${whichPlayer}` &&
+        whichPlayer === whichPlayer
+      ) {
+        hitValue[whichPlayer] = 20;
+        console.log(`(Player ${whichPlayer}) selected the strength buff`);
+        buffStrength[whichPlayer] = true;
+        buffControl();
+        nextPlayer();
+      }
+    });
+  }
+}
+
+//****************************
+/*Buff conditions are checked by the functions below.
+  buffControl(), by default will check the conditions for health
+  and will always call the two other functions to check conditions
+  for shield and strength
+  /****************************/
+
+function buffControl() {
+  if (buffHeart[whichPlayer] === true) {
+    document.getElementById(`heart-${whichPlayer}`).disabled = true;
+
+    healthCount[whichPlayer]++;
+
+    if (healthCount[whichPlayer] === 33) {
+      buffHeart[whichPlayer] = false;
+
+      document.getElementById(`heart-${whichPlayer}`).disabled = false;
+
+      document
+        .getElementById(`heart-${whichPlayer}`)
+        .classList.remove("inactiveBuff");
+
+      healthCount[whichPlayer] = 0;
+    }
+  }
+  shieldControl(); // conditions in these functions are checked on every button click
+  strengthControl(); //*****************************************************************/
+  
+  if (buffShield[whichPlayer] === true) { // changes the active mini buff icons dynamically; defaults located under resetGame()
+    document
+      .getElementById(`shield-current-${whichPlayer}`)
+      .classList.remove("inactiveBuff");
+  } else {
+    document
+      .getElementById(`shield-current-${whichPlayer}`)
+      .classList.add("inactiveBuff");
+  }
+  if (buffStrength[whichPlayer] === true) {
+    document
+      .getElementById(`strength-current-${whichPlayer}`)
+      .classList.remove("inactiveBuff");
+  } else {
+    document
+      .getElementById(`strength-current-${whichPlayer}`)
+      .classList.add("inactiveBuff");
+  }
+}
+
+function shieldControl() {
+  if (buffShield[whichPlayer] === true) {
+    document.getElementById(`shield-${whichPlayer}`).disabled = true;
+
+    shieldCount[whichPlayer]++;
+
+    if (shieldCount[whichPlayer] === 9) {
+      //after 9 total turns; shield buff no longer active; button still inactive for 33 turns
+
+      if (whichPlayer === 0) {
+        hitValue[1] = 10;
+      } else {
+        hitValue[0] = 10; // reinstate original hit value for opposing player
+      }
+      console.log(`Shield no longer active`);
+    } else if (shieldCount[whichPlayer] === 33) {
+      // make shield buff available to use again
+      shieldCount[whichPlayer] = 0;
+      buffShield[whichPlayer] === false;
+      document.getElementById(`shield-${whichPlayer}`).disabled = false;
+      document
+        .getElementById(`shield-${whichPlayer}`)
+        .classList.remove("inactiveBuff");
+    }
+  }
+}
+
+function strengthControl() {
+  if (buffStrength[whichPlayer] === true) {
+    document.getElementById(`strength-${whichPlayer}`).disabled = true; //button disabled
+    document
+      .getElementById(`strength-${whichPlayer}`)
+      .classList.add("inactiveBuff");
+
+    strengthCount[whichPlayer]++;
+    // as above; after 9 turns, buff no longer active
+    if (strengthCount[whichPlayer] === 9) {
+      buffStrength[whichPlayer] = false;
+
+      document.getElementById(`strength-${whichPlayer}`).disabled = false;
+
+      if (whichPlayer === 0) {
+        hitValue[whichPlayer] = 10;
+      } else {
+        hitValue[whichPlayer] = 10;
+      }
+      if (strengthCount[whichPlayer] === 33) {
+        strengthCount[whichPlayer] = 0;
+        buffStrength = false;
+        document
+          .getElementById(`strength-${whichPlayer}`)
+          .classList.remove("inactiveBuff");
+      }
+    }
   }
 }
