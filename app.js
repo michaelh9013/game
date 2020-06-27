31471e645a09a41f8cd7d6fc276001603d9ac95d
michaelh9013 <64647620+michaelh9013@users.noreply.github.com>
2020-06-26 20:10:26 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // buff buttons need to be disabled when game ends
-
-// features to add: no buffs allowed //When reset button pressed mult times, attack buttons disabledonce attack takes place, per turn.
+// features to add:
+//When reset button pressed mult times, attack buttons disabledonce attack takes place, per turn.
 //new UI... more compact
 //allow health buff to take effect over time
-// correct time outs for active buffs
-//buffs need to be disabled during threeTurns()
+
+
 
 let health,
   autoAttack,
+  choice,
   noBuffsThisTurn,
   activeGame,
   activeBuffs,
@@ -23,6 +24,7 @@ let health,
   hitValue,
   healthCount,
   shieldCount,
+  isArtificial,
   strengthCount;
 
 resetGame(); //load all game presets
@@ -33,6 +35,16 @@ document
 
 document.getElementById("reset").addEventListener("click", resetGame); // reset button
 
+//player selects to play against the CPU; non selected player now is CPU
+document.getElementById(`CPU`).addEventListener("click", function () {
+  if (whichPlayer === 0) {
+    document.getElementById(`h2-player-1`).textContent = "CPU";
+    isArtificial[1] = true;
+  } else {
+    document.getElementById(`h2-player-0`).textContent = "CPU";
+    isArtificial[0] = true;
+  }
+});
 /*****************
  *
  * Attack button is pressed
@@ -150,6 +162,7 @@ function resetGame() {
   buffHeart = [false, false];
   buffShield = [false, false];
   buffStrength = [false, false];
+  isArtificial = [false, false];
   hitValue = [10, 10];
   healthCount = [0, 0];
   shieldCount = [0, 0];
@@ -196,7 +209,6 @@ function resetGame() {
   for (var i = 0; i < activeBuffs.length; i++) {
     activeBuffs[i].classList.add("inactiveBuff");
   }
-  toggleButtons();
 }
 /************
  *
@@ -205,8 +217,6 @@ function resetGame() {
  ***********/
 
 function nextPlayer() {
-  clearInterval(autoAttack); // stop auto attack of previous player
-
   if (whichPlayer === 0) {
     whichPlayer++;
 
@@ -229,12 +239,20 @@ function nextPlayer() {
     document.getElementById(`player-1`).classList.remove("activePlayer");
   }
 
+  clearInterval(autoAttack); // stop auto attack of previous player
   turnCount = 0;
   document.getElementById(`reset`).disabled = false; // re-enable reset button for next player
-  for (var i = 0; i < buffs.length; i++) {//re-enable buffs 
+  for (var i = 0; i < buffs.length; i++) {
+    //re-enable buffs
     buffs[i].disabled = false;
   }
+
   console.log(`nextPlayer() called, var whichPlayer = ${whichPlayer}.`);
+
+  if (isArtificial[whichPlayer] === true) {
+    //check to see if opponent is using the AI
+    artificialPlayer();
+  }
 }
 
 /*************
@@ -249,30 +267,12 @@ function endGame() {
     .classList.remove("activePlayer"); // remove activePlayer class
   document.getElementById(`player-${whichPlayer}`).classList.add("winner"); // apply winner class to winning player
   document.getElementById(`h2-player-${whichPlayer}`).textContent = "Winner!";
-
-  toggleButtons();
+  document.getElementById(`reset`).disabled = false;
+  document.getElementById(`attack-0`).disabled = true;
+  document.getElementById(`attack-1`).disabled = true;
 
   console.log(`endGame(), GAME OVER!`);
   return;
-}
-
-/***************
- *
- * Toggle the Buttons
- *
- ********/
-
-function toggleButtons() {
-  if (whichPlayer === 0 && health[1] > 0) {
-    // toggles buttons at beginning of game
-    document.getElementById(`attack-1`).disabled = true;
-  } else if (whichPlayer === 1 && health[0] > 0) {
-    document.getElementById(`attack-0`).disabled = true;
-  } else if (health[0] === 0 || health[1] === 0) {
-    document.getElementById(`attack-0`).disabled = true;
-    document.getElementById(`attack-1`).disablde = true;
-    return;
-  }
 }
 
 function getBuffs() {
@@ -290,9 +290,7 @@ function getBuffs() {
           .setAttribute("value", health[whichPlayer]);
         document.getElementById(`health-num-${whichPlayer}`).textContent =
           health[whichPlayer];
-        document
-          .getElementById(`heart-${whichPlayer}`)
-          .classList.add("inactiveBuff");
+
         buffHeart[whichPlayer] = true;
         buffControl();
         nextPlayer();
@@ -312,9 +310,7 @@ function getBuffs() {
           }
         }
         console.log(`(Player ${whichPlayer}) selected the sheild buff`);
-        document
-          .getElementById(`shield-${whichPlayer}`)
-          .classList.add("inactiveBuff");
+
         buffShield[whichPlayer] = true;
         buffControl();
         nextPlayer();
@@ -340,34 +336,12 @@ function getBuffs() {
 
 //****************************
 /*Buff conditions are checked by the functions below.
-  buffControl(), by default will check the conditions for health
-  and will always call the two other functions to check conditions
-  for shield and strength
+ 
   /****************************/
 
 function buffControl() {
-  if (buffHeart[whichPlayer] === true) {
-    document.getElementById(`heart-${whichPlayer}`).disabled = true;
-
-    healthCount[whichPlayer]++;
-
-    if (healthCount[whichPlayer] === 33) {
-      buffHeart[whichPlayer] = false;
-
-      document.getElementById(`heart-${whichPlayer}`).disabled = false;
-
-      document
-        .getElementById(`heart-${whichPlayer}`)
-        .classList.remove("inactiveBuff");
-
-      healthCount[whichPlayer] = 0;
-    }
-  }
-  shieldControl(); // conditions in these functions are checked on every button click
-  strengthControl(); //*****************************************************************/
-
+  // changes the active mini buff icons dynamically; defaults located under resetGame()
   if (buffShield[whichPlayer] === true) {
-    // changes the active mini buff icons dynamically; defaults located under resetGame()
     document
       .getElementById(`shield-current-${whichPlayer}`)
       .classList.remove("inactiveBuff");
@@ -385,27 +359,55 @@ function buffControl() {
       .getElementById(`strength-current-${whichPlayer}`)
       .classList.add("inactiveBuff");
   }
+  shieldControl(); // conditions in these functions are checked on every button click
+  strengthControl(); //*****************************************************************/
+  healthControl();
 }
 
+//check conditions for health buff
+function healthControl() {
+  if (buffHeart[whichPlayer] === true) {
+    document.getElementById(`heart-${whichPlayer}`).disabled = true; //icon can not be selected
+    document
+      .getElementById(`heart-${whichPlayer}`)
+      .classList.add("inactiveBuff"); // make icon clear
+    healthCount[whichPlayer]++;
+  }
+
+  if (healthCount[whichPlayer] >= 20) {
+    buffHeart[whichPlayer] = false;
+    healthCount[whichPlayer] = 0;
+
+    document.getElementById(`heart-${whichPlayer}`).disabled = false;
+
+    document
+      .getElementById(`heart-${whichPlayer}`)
+      .classList.remove("inactiveBuff");
+  }
+}
+
+//check conditions for shield buff
 function shieldControl() {
   if (buffShield[whichPlayer] === true) {
     document.getElementById(`shield-${whichPlayer}`).disabled = true;
+    document
+      .getElementById(`shield-${whichPlayer}`)
+      .classList.add("inactiveBuff"); //make icon clear
 
     shieldCount[whichPlayer]++;
+  }
+  if (shieldCount[whichPlayer] >= 9) {
+    shieldCount[whichPlayer]++;
+    if (whichPlayer === 0) {
+      hitValue[1] = 10;
+    } else {
+      hitValue[0] = 10; // reinstate original hit value for opposing player
+    }
 
-    if (shieldCount[whichPlayer] === 9) {
-      //after 9 total turns; shield buff no longer active; button still inactive for 33 turns
-
-      if (whichPlayer === 0) {
-        hitValue[1] = 10;
-      } else {
-        hitValue[0] = 10; // reinstate original hit value for opposing player
-      }
-      console.log(`Shield no longer active`);
-    } else if (shieldCount[whichPlayer] === 33) {
-      // make shield buff available to use again
+    //after 15 turns, buff icon available for use again; set to false
+    if (shieldCount[whichPlayer] >= 15) {
+      buffShield[whichPlayer] = false;
       shieldCount[whichPlayer] = 0;
-      buffShield[whichPlayer] === false;
       document.getElementById(`shield-${whichPlayer}`).disabled = false;
       document
         .getElementById(`shield-${whichPlayer}`)
@@ -414,32 +416,180 @@ function shieldControl() {
   }
 }
 
+//check conditions for strength buff 
 function strengthControl() {
+  //when buffStrength is true
   if (buffStrength[whichPlayer] === true) {
     document.getElementById(`strength-${whichPlayer}`).disabled = true; //button disabled
     document
       .getElementById(`strength-${whichPlayer}`)
-      .classList.add("inactiveBuff");
+      .classList.add("inactiveBuff"); //make icon clear
 
     strengthCount[whichPlayer]++;
-    // as above; after 9 turns, buff no longer active
-    if (strengthCount[whichPlayer] === 9) {
+    console.log(`str count:${strengthCount[whichPlayer]}`);
+  }
+  // as above; after 9 turns, buff no longer active;
+  if (strengthCount[whichPlayer] >= 9) {
+    strengthCount[whichPlayer]++;
+    if (whichPlayer === 0) {
+      hitValue[whichPlayer] = 10;
+    } else {
+      hitValue[whichPlayer] = 10;
+    }
+
+    //after 15 turns, buff icon available for use again; set to false
+    if (strengthCount[whichPlayer] >= 15) {
       buffStrength[whichPlayer] = false;
-
-      document.getElementById(`strength-${whichPlayer}`).disabled = false;
-
-      if (whichPlayer === 0) {
-        hitValue[whichPlayer] = 10;
-      } else {
-        hitValue[whichPlayer] = 10;
-      }
-      if (strengthCount[whichPlayer] === 33) {
-        strengthCount[whichPlayer] = 0;
-        buffStrength = false;
-        document
-          .getElementById(`strength-${whichPlayer}`)
-          .classList.remove("inactiveBuff");
-      }
+      strengthCount[whichPlayer] = 0;
+      document
+        .getElementById(`strength-${whichPlayer}`)
+        .classList.remove("inactiveBuff");
+      console.log(`str count:${strengthCount[whichPlayer]}`);
     }
   }
+}
+
+/*********
+ *
+ * AI
+ * Playing against the computer
+ */
+
+function artificialPlayer() {
+  choice = Math.floor(Math.random() * 100);
+
+  console.log(`${health[whichPlayer]} health`);
+  console.log(choice);
+
+  //check health
+  if (health[whichPlayer] === 100) {
+    if (choice > 10 && choice <= 45) {
+      artificialStrength(); //choose strength
+    } else if (choice > 45) {
+      artificialShield(); //choose shield
+    } else {
+      // up to 10% chance of AI attacking
+      attack();
+    }
+  }
+  //check health between 75 && 100
+  if (health[whichPlayer] >= 75 && health[whichPlayer] < 100) {
+    if (choice <= 33) {
+      attack();
+    } else if (choice > 33 && choice < 66) {
+      artificialStrength();
+    } else if (choice >= 66) {
+      artificialShield();
+    }
+  }
+  //check health between 50 && 75
+  if (health[whichPlayer] >= 50 && health[whichPlayer] < 75) {
+    if (choice <= 10) {
+      //10% chance of selecting to heal itself
+      artificialHealth();
+    } else if (choice > 10 && choice <= 40) {
+      attack();
+    } else if (choice > 40 && choice <= 70) {
+      artificialStrength();
+    } else if (choice > 70) {
+      artificialShield();
+    }
+  }
+  //check health between 25 && 50
+  if (health[whichPlayer] >= 25 && health[whichPlayer] < 50) {
+    if (choice <= 30) {
+      artificialHealth();
+    } else if (choice > 30 && choice <= 53) {
+      attack();
+    } else if (choice > 53 && choice <= 76) {
+      artificialStrength();
+    } else if (choice > 76) {
+      artificialStrength();
+    }
+  }
+  //check health between 10 && 25
+  if (health[whichPlayer] >= 10 && health[whichPlayer] < 25) {
+    if (choice <= 75) {
+      artificialHealth();
+    } else if (choice > 75 && choice <= 83) {
+      attack();
+    } else if (choice > 83 && choice <= 91) {
+      artificialStrength();
+    } else if (choice > 91) {
+      artificialStrength();
+    }
+  }
+
+  //check health below 10
+  if (health[whichPlayer] < 10) {
+    artificialHealth();
+  }
+}
+
+//function used by AI to determine which action to take based on num
+
+//AI chose health buff
+function artificialHealth() {
+  // if buffHeart is active; attempt to apply the shield else attack opponent
+  //************this section is buggy********/
+  if (buffHeart[whichPlayer] === true && buffShield[whichPlayer] === true) {
+    attack();
+    console.log(`AI has chosen to attack`);
+    return;
+  } else if (
+    buffHeart[whichPlayer] === true &&
+    buffShield[whichPlayer] === false
+  ) {
+    buffShield[whichPlayer] === true;
+    console.log(`AI has chosen to apply the shield`);
+  }
+
+  buffHeart[whichPlayer] = true;
+  document.getElementById(`heart-${whichPlayer}`).classList.add("inactiveBuff");
+  health[whichPlayer] += 50;
+  if (health[whichPlayer] > 100) {
+    //if health is over 100, set it to 100
+    health[whichPlayer] = 100;
+  }
+  document
+    .getElementById(`meter-${whichPlayer}`)
+    .setAttribute("value", health[whichPlayer]);
+  document.getElementById(`health-num-${whichPlayer}`).textContent =
+    health[whichPlayer]; // health numerical display
+  console.log(`AI has chosen to heal itself`);
+  buffControl();
+  nextPlayer();
+}
+
+//AI chose str buff
+function artificialStrength() {
+  if (buffStrength[whichPlayer] === true) {
+    attack();
+    console.log(`AI has selected attack`);
+    return;
+  }
+
+  //make stength buff active
+  buffStrength[whichPlayer] = true;
+  document
+    .getElementById(`strength-${whichPlayer}`)
+    .classList.add("inactiveBuff");
+  console.log(`AI chose Str`);
+  buffControl();
+  nextPlayer();
+}
+
+//AI chose shield buff
+function artificialShield() {
+  if (buffShield[whichPlayer] === true) {
+    attack();
+    console.log(`AI has selected attack`);
+    return;
+  }
+
+  //make shield buff active
+  buffShield[whichPlayer] = true;
+  console.log(`AI chose shield`);
+  buffControl();
+  nextPlayer();
 }
